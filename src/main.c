eba58fa260797cab62d91d491ed40fac1f706610
XoXoGameWolf <xoxogamewolf@gmail.com>
2025-08-17 11:04:19 -0400

    added more animation

--- src/main.c
+++ src/main.c
@@ -5,25 +5,21 @@ float bg_blue = 0.9f;
 
 #include <coco/coco.c>
 
-Object* object1;
-Object* object2;
-Object* object3;
+Object* object;
 
 void start() {
     Mesh* sphere = loadMesh("resources/sphere.obj");
 
-    object1 = createBasicShaded3D(sphere, -2.5f, 0.0f, 5.0f, 0.0f, 0.0f, 0.0f, 1.0f, 1.0f, 1.0f);
-    setColorProperty(object1, 0.8f, 0.2f, 0.2f, "col");
-
-    object2 = createBasicShaded3D(sphere, 0.0f, 0.0f, 5.0f, 0.0f, 0.0f, 0.0f, 1.0f, 1.0f, 1.0f);
-    setColorProperty(object2, 0.2f, 0.8f, 0.2f, "col");
-
-    object3 = createBasicShaded3D(sphere, 2.5f, 0.0f, 5.0f, 0.0f, 0.0f, 0.0f, 1.0f, 1.0f, 1.0f);
-    setColorProperty(object3, 0.2f, 0.2f, 0.8f, "col");
+    object = createBasicShaded3D(sphere, 0.0f, 0.0f, 5.0f, 0.0f, 0.0f, 0.0f, 1.0f, 1.0f, 1.0f);
 }
 
 void update() {
-    //object->rot_x = glfwGetTime() * 100.0f;
-    //object->rot_y = glfwGetTime() * 100.0f + 10.0f;
-    //object->rot_z = glfwGetTime() * 100.0f + 20.0f;
+    object->pos_x = 2.5f * sin(3.0f * glfwGetTime());
+    object->rot_y = 100.0f * glfwGetTime();
+    setColorProperty(object, 
+        sin(3.0f * glfwGetTime()) / 2.0f + 0.5f, 
+        sin(3.0f * glfwGetTime() + 10.0f) / 2.0f + 0.5f, 
+        sin(3.0f * glfwGetTime() + 20.0f) / 2.0f + 0.5f,
+        "col"
+    );
 }
